a44cdb83e339cc7c9f5680f7f07f7c6fd0f51ca3
skopu <sebastian.skopp@gmail.com>
2014-12-16 21:06:31 +0100

    add generators module for generator

--- lib/generators/sofort/sofort_generator.rb
+++ lib/generators/sofort/sofort_generator.rb
@@ -1,17 +1,19 @@
 require 'rails/generators/base'
 
 module Sofort
-  class InstallGenerator < Rails::Generators::Base
-    source_root File.expand_path("../../templates", __FILE__)
+  module Generators
+    class InstallGenerator < Rails::Generators::Base
+      source_root File.expand_path("../../templates", __FILE__)
 
-    desc "Creates a Sofort initializer to your application."
+      desc "Creates a Sofort initializer to your application."
 
-    def copy_initializer
-      template "sofort_rails.rb", "config/initializers/sofort.rb"
-    end
+      def copy_initializer
+        template "sofort_rails.rb", "config/initializers/sofort.rb"
+      end
 
-    def show_readme
-      readme "README" if behavior == :invoke
+      def show_readme
+        readme "README" if behavior == :invoke
+      end
     end
   end
 end
