0a88067d95f45cc4b108970fd1160d7064e94fba
Simon Schoeters <hamfilter@gmail.com>
2015-09-29 15:18:42 +0200

    Allow client to set the language code

--- lib/generators/templates/sofort.rb
+++ lib/generators/templates/sofort.rb
@@ -1,19 +1,18 @@
 Sofort.setup do |config|
 
-  config.base_url = "https://api.sofort.com/api/xml"
+  config.base_url = 'https://api.sofort.com/api/xml'
   config.user_id = 'sofort_user_id'
   config.api_key = 'api_key'
   config.abort_url = 'abort_url'
   config.success_url = 'success_url'
   config.notification_url = 'notification_url'
-
+  config.language_code = 'de'
   config.email_customer = 'email_customer'
   config.notification_email = 'notification_email'
   config.project_id = 'project_id'
-  config.country_code = "DE"
-  config.currency_code = "EUR"
-  config.reason = "Reason"
-
+  config.country_code = 'DE'
+  config.currency_code = 'EUR'
+  config.reason = 'Reason'
   config.user_variable = 'user_variable'
 
 end
